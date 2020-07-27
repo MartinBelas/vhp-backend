363aff207dbb7d9f5990a985dd1d1672a1805fbb
Martin <Martin.Belas@outlook.com>
2020-07-27 23:15:07 +0200

    Add response headers. Remove bodyparser dependency.

--- src/app.js
+++ src/app.js
@@ -13,11 +13,11 @@ const UsersRouter = require('./users/users-routes.js');
 const CompetitionsRouter = require('./competitions/competitions.routes.js');
 
 app.use(function (req, res, next) {
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Credentials', 'true');
-    // res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    // res.header('Access-Control-Expose-Headers', 'Content-Length');
-    // res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     if (req.method === 'OPTIONS') {
         return res.send(200);
     } else {
@@ -25,9 +25,8 @@ app.use(function (req, res, next) {
     }
 });
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json()); 
 
 // AuthorizationRouter.routesConfig(app);
 UsersRouter(app);
