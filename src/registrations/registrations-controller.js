29ce297eb74dd76fbc03e39d420628939dd858da
Martin Belas <Martin.Belas@outlook.com>
2021-02-28 00:38:10 +0100

    refactor: registration controller

--- src/registrations/registrations-controller.js
+++ src/registrations/registrations-controller.js
@@ -3,46 +3,70 @@
 const RegistrationsDao = require('./registrations-dao-mysql');
 const { RegistrationBuilder, Sex } = require('./registration');
 const YearsDao = require('../years/years-dao-mysql');
+const ApiKeyService = require('../common/ApiKeyService');
+const EmailService = require('../common/EmailService');
+
+const registration_subject = "Registrace na VH pulmaraton"
+
+function getRegistrationMailBody(newRegistration) {
+    
+    return `
+        Jméno: ${newRegistration.firstName} 
+        Příjmení: ${newRegistration.lastName} 
+        E-mail: ${newRegistration.email} 
+        Rok narození: ${newRegistration.birth} 
+        Závod: ${newRegistration.race}
+
+        Děkujeme za registraci a těšíme se na Vaši účast.
+        Registraci prosím dokončete zaplacením registračního poplatku 250 Kč na účet 2200743991/2010, děkujeme.
+
+        Pro případné dotazy nás neváhejte kontaktovat, viz www.vh-pulmaraton.cz/kontakty
+        `;
+}
 
 module.exports = class RegistrationsController {
 
-    static getNextYear = async function(competition) {
+    static getNextYear = async function (competition) {
 
-        const yearsDao = new YearsDao();
-        let nextYearDateYear;
-
-        if (!nextYearDateYear) {
-            const nextYear = (await yearsDao.findNextYear(competition))
-            nextYearDateYear = nextYear.date.substring(0,4);
+        if (ApiKeyService.getApiKeyOk()) {
+            const yearsDao = new YearsDao();
+            let nextYearDateYear;
+    
+            if (!nextYearDateYear) {
+                const nextYear = (await yearsDao.findNextYear(competition))
+                nextYearDateYear = nextYear.date.substring(0, 4);
+            }
+    
+            return nextYearDateYear;
         }
-
-        return nextYearDateYear;
     }
 
     static getAll = async function (req, res) {
 
-        const competition = req.params.competition;
-        
-        const nextYear = await RegistrationsController.getNextYear(competition);
-
-        if (nextYear.error) {
-            res.status(nextYear.suggestedStatus).send({ message: nextYear.error });
-            return;
+        if (ApiKeyService.getApiKeyOk()) {
+            const competition = req.params.competition;
+    
+            const nextYear = await RegistrationsController.getNextYear(competition);
+    
+            if (nextYear.error) {
+                res.status(nextYear.suggestedStatus).send({ message: nextYear.error });
+                return;
+            }
+    
+            const dao = new RegistrationsDao(nextYear);
+    
+            dao.find(competition)
+                .then(data => {
+                    data.forEach(element => { element.email = "" });
+                    data.forEach(element => { element.birth = "" });
+                    data.forEach(element => { element.phone = "" });
+                    res.json(data);
+                })
+                .catch(err => {
+                    console.error(err);
+                    RegistrationsController.responseWithDbConnectionError(res);
+                })
         }
-
-        const dao =  new RegistrationsDao(nextYear);
-
-        dao.find(competition)
-            .then(data => {
-                data.forEach(element => { element.email = "" });
-                data.forEach(element => { element.birth = "" });
-                data.forEach(element => { element.phone = "" });
-                res.json(data);
-            })
-            .catch(err => {
-                console.error(err);
-                RegistrationsController.responseWithDbConnectionError(res);
-            })
     };
 
     //TODO for adm only
@@ -69,72 +93,80 @@ module.exports = class RegistrationsController {
         const competition = req.params.competition;
 
         const nextYear = await RegistrationsController.getNextYear(competition);
-        
+
         if (nextYear.error) {
             res.status(nextYear.suggestedStatus).send({ message: nextYear.error });
             return;
         }
-        
-        const dao =  new RegistrationsDao(nextYear);
+
+        const dao = new RegistrationsDao(nextYear);
 
         // Validate request
-        if (!req.body.email) {
+        if (!req.body.registration.email) {
             res.status(400).send({ message: "Email can not be empty!" });
             return;
         }
-        if (!req.body.firstname) {
+        if (!req.body.registration.firstname) {
             res.status(400).send({ message: "First name can not be empty!" });
             return;
         }
-        if (!req.body.lastname) {
+        if (!req.body.registration.lastname) {
             res.status(400).send({ message: "Last name can not be empty!" });
             return;
         }
-        if (!req.body.birth) {
+        if (!req.body.registration.birth) {
             res.status(400).send({ message: "Birth year can not be empty!" });
             return;
         }
-        if (!req.body.sex) {
+        if (!req.body.registration.sex) {
             res.status(400).send({ message: "Sex can not be empty!" });
             return;
         }
-        let sex = req.body.sex;
+        let sex = req.body.registration.sex;
         if (!(['M', 'F'].includes(sex))) {
             res.status(400).send({ message: "Sex must be 'M' or 'F' only!" });
             return;
         }
         sex = Sex[sex];
 
-        if (!req.body.race) {
+        if (!req.body.registration.race) {
             res.status(400).send({ message: "Race can not be empty!" });
             return;
         }
 
-        let newRegistration = new RegistrationBuilder()
-            .setEmail(req.body.email)    
-            .setFirstName(req.body.firstname)
-            .setLastName(req.body.lastname)
-            .setBirth(req.body.birth)
-            .setSex(sex)
-            .setAddress(req.body.home)
-            .setPhone(req.body.phone)
-            .setClub(req.body.club)
-            .setRace(req.body.race)
-            .setComment(req.body.notes)
-            .setPaid(false)
-            
-            //TODO category, timeStamp
-            .build();
+        let newRegistration;
+        try {
+            let registration = req.body.registration;
+            newRegistration = new RegistrationBuilder()
+                .setEmail(registration.email)
+                .setFirstName(registration.firstname)
+                .setLastName(registration.lastname)
+                .setBirth(registration.birth)
+                .setSex(sex)
+                .setAddress(registration.home)
+                .setPhone(registration.phone)
+                .setClub(registration.club)
+                .setRace(registration.race)
+                .setComment(registration.notes)
+                .setPaid(false)
+    
+                //TODO category, timeStamp
+                .build();
+        } catch (err) {
+            console.log('RegistrationsController create: ', err);
+            res.status(422);
+            res.send(err);
+        }
 
-        console.log("NEW REG newRegistration: ", newRegistration); //TODO rm
 
         dao.create(req.params.competition, newRegistration)
             .then(data => {
-                console.log("NEW REG result: ", data); //TODO rm
+                EmailService.sendMail(newRegistration.email, registration_subject, getRegistrationMailBody(newRegistration));
                 res.json(data);
             })
             .catch(err => {
-                console.error("Create registration error: ", err);
+                console.error("ERR - Create registration error: ", err);
+                res.status(500);
                 res.send(err);
             })
     };
@@ -177,7 +209,7 @@ module.exports = class RegistrationsController {
     static responseWithDbConnectionError = function (res) {
         res.status(503);
         res.send({
-            "message":"Error when connecting to db."
+            "message": "Error when connecting to db."
         });
     };
 }
