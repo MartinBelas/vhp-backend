77a9d5a7e1e019a4074508ab496a35f567871756
Martin Belas <Martin.Belas@outlook.com>
2022-05-02 19:05:11 +0200

    Try to remove FE folder

---
+++ run.bat
@@ -0,0 +1,13 @@
+
+ENV=$1
+
+if [[ -z "$ENV" ]]
+then
+    echo "You must provide 'NODE_ENV' value as parameter!"
+    echo "Can't start without it, try it again..."
+    exit -1
+fi
+
+echo "Starting vhp-backend server with env: $ENV"
+
+NODE_ENV=$ENV npm start
